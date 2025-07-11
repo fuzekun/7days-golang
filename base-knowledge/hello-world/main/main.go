bd31722ee4b136ede437266e4b9885d651c4a072
fuzekun <1078682405@qq.com>
2025-07-11 21:01:03 +0800

    fit(格式)：解决两个未解决的异常

--- base-knowledge/hello-world/main/main.go
+++ base-knowledge/hello-world/main/main.go
@@ -11,7 +11,10 @@ func main() {
 	fmt.Println("Please visit http://127.0.0.1:8080/")
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		s := fmt.Sprintf("你好, 世界! -- Time: %s", time.Now().String())
-		fmt.Fprintf(w, "%v\n", s)
+		_, err := fmt.Fprintf(w, "%v\n", s)
+		if err != nil {
+			return
+		}
 		log.Printf("%v\n", s)
 	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
